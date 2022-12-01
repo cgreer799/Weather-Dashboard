a5dbaec7af330ef8ff42fa12729cf158ab69d7b1
cgreer799 <cgreer799@gmail.com>
2022-11-30 20:55:32 -0600

    first test update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let apikey = "91c8754679a350959d15296d5f38bd1a"
 function search(){
     var searchinput = document.getElementById("searchinput").ariaValueText;
 
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + searchinput + ',US&limit=5&appid=' + apikey)
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + searchinput + ',US&limit=5&appid=' + apikey+"&units=imperial")
     .then(function (response){
         const j = response.json();
         return j;
@@ -11,12 +11,20 @@ function search(){
     let lat = pj.lat;
     let lon = pj.lon;
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apikey)
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apikey+"&units=imperial")
     .then(function (response){
         const forecastjson = response.json();
         return forecastjson;
     });
-    
+    const forecast = JSON.parse(forecastjson);
+    let f = forecast.list;
+    var currdate = new Date(f[0].dt_txt);
+
+    document.getElementById("city&date&icon").innerText = searchinput+' ('+currdate.getMonth()+"/"+currdate.getDay()+"/"+currdate.getFullYear+") "+
+    "http://openweathermap.org/img/w/" + f[0].Weather[0].icon + ".png";
+    document.getElementById("currtemp").innerText = "Temp:" +f[0].main.temp+"°F";
+    document.getElementById("currwind").innerText = "Wind:" +f[0].wind.speed+" MPH";
+    document.getElementById("currhumidity").innerText = "Humidity: "+ f[0].main.humidity+" %";
 };
 
 
